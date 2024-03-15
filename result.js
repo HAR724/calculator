e9ac1e0d999ce5877ea70ca7c9feb7f9b35cd9df
HAR724 <harshbarsaiyan123@gmail.com>
2024-03-15 15:35:01 +0530

    first commit

---
+++ result.js
@@ -0,0 +1,69 @@
+document.frm.n.disabled=true
+document.frm.n.value=""
+document.frm.a.disabled=true
+document.frm.b.disabled=true
+document.frm.c.disabled=true
+document.frm.d.disabled=true
+document.frm.f.disabled=true
+document.frm.g.disabled=true
+document.frm.h.disabled=true
+document.frm.i.disabled=true
+document.frm.j.disabled=true
+document.frm.k.disabled=true
+document.frm.l.disabled=true
+document.frm.m.disabled=true
+document.frm.o.disabled=true
+document.frm.p.disabled=true
+document.frm.q.disabled=true
+document.frm.r.disabled=true
+document.frm.s.disabled=true
+
+function fun1(){
+    document.frm.n.disabled=true
+    document.frm.n.value=""
+    document.frm.a.disabled=false
+    document.frm.b.disabled=false
+    document.frm.c.disabled=false
+    document.frm.d.disabled=false
+    document.frm.f.disabled=false
+    document.frm.g.disabled=false
+    document.frm.h.disabled=false
+    document.frm.i.disabled=false
+    document.frm.j.disabled=false
+    document.frm.k.disabled=false
+    document.frm.l.disabled=false
+    document.frm.m.disabled=false
+    document.frm.o.disabled=false
+    document.frm.p.disabled=false
+    document.frm.q.disabled=false
+    document.frm.r.disabled=false
+    document.frm.s.disabled=false
+}
+
+function fun2(){
+    document.frm.n.disabled=true
+    document.frm.n.value=""
+    document.frm.a.disabled=true
+    document.frm.b.disabled=true
+    document.frm.c.disabled=true
+    document.frm.d.disabled=true
+    document.frm.f.disabled=true
+    document.frm.g.disabled=true
+    document.frm.h.disabled=true
+    document.frm.i.disabled=true
+    document.frm.j.disabled=true
+    document.frm.k.disabled=true
+    document.frm.l.disabled=true
+    document.frm.m.disabled=true
+    document.frm.o.disabled=true
+    document.frm.p.disabled=true
+    document.frm.q.disabled=true
+    document.frm.r.disabled=true
+    document.frm.s.disabled=true
+
+}
+
+function fun3(cat){
+    var x=cat
+   document.frm.n.value= x.slice(0,-1)
+    }
